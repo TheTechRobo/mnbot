1e816aebd12f0d3878f43ef405847c41c8f594a4
TheTechRobo <thetechrobo@proton.me>
2025-05-10 09:38:47 -0400

    Worker version 20250510.01. Install emoji-capable font.

--- client/worker/shared.py
+++ client/worker/shared.py
@@ -7,7 +7,7 @@ logging.basicConfig(format = "%(asctime)s %(levelname)s <:%(thread)s> : %(messag
 # Update this whenever you make a change, cosmetic or not.
 # During development you can ignore it, but when you actually
 # push it to prod, it *must* be updated.
-VERSION = "20250509.01"
+VERSION = "20250510.01"
 
 DEBUG = os.environ.get("DEBUG") == "1"
 if DEBUG:
